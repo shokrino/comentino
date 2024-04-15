948d8ad8ecfac2b6af3afdc3c904d8d6f1a482d4
Matin Shokri <shokrino.wp@gmail.com>
2024-04-16 02:17:40 +0330

    initial commit.

---
+++ comentino.php
@@ -0,0 +1,11 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+/*
+Plugin Name: Comentino
+Plugin URI: https://shokrino.com/
+Description: Style the comments in wordpress fast and easy
+Author: Shokrino Team | Nias
+Version: 1.0.0
+Author URI: https://nias.ir
+Textdomain: comentino
+*/
